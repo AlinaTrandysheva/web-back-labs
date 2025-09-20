37ff6b60c9b2c8fd4f868e6b19d024d55f77bd52
Alina Trandysheva <trandyshevaalina@gmail.com>
2025-09-20 10:56:01 +0300

    Имя студента

--- app.py
+++ app.py
@@ -361,6 +361,7 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Алина '
+    return render_template('example.html', name=name)
     
 
