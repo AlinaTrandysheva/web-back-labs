07e71e1baa11ee421c0e2af026ba69024790c479
Alina Trandysheva <trandyshevaalina@gmail.com>
2025-12-25 16:18:26 +0300

    Модель базы данных

--- app.py
+++ app.py
@@ -6,13 +6,33 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 import datetime
-import os 
+import os
+from os import path
+from db import db
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'alina_trandysheva_orm'
+    db_user = 'alina_trandysheva_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+    )
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'alina_trandysheva_orm.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -21,6 +41,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 @app.route("/")
 @app.route("/index")
@@ -41,6 +62,7 @@ def index():
                 <li><a href="/lab5/">Пятая лабораторная</a></li>
                 <li><a href="/lab6/">Шестая лабораторная</a></li>
                 <li><a href="/lab7/">Седьмая лабораторная</a></li>
+                <li><a href="/lab8/">Восьмая лабораторная</a></li>
             </ul>
         </nav>
         <footer>
