1df80d37f151b10c9fccb92de6ee1755ab586d92
Alina Trandysheva <trandyshevaalina@gmail.com>
2025-12-25 19:20:22 +0300

    Правки

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 import datetime
 import os
 from os import path
@@ -24,7 +25,7 @@ def load_users(login_id):
     return users.query.get(int(login_id))
 
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'super-secret-key-123')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
 if app.config['DB_TYPE'] == 'postgres':
@@ -52,6 +53,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 @app.route("/index")
@@ -73,6 +75,7 @@ def index():
                 <li><a href="/lab6/">Шестая лабораторная</a></li>
                 <li><a href="/lab7/">Седьмая лабораторная</a></li>
                 <li><a href="/lab8/">Восьмая лабораторная</a></li>
+                <li><a href="/lab9/">Девятая лабораторная</a></li>
             </ul>
         </nav>
         <footer>
